ad5786ac98cdb513f758c0e193c6437317b5f750
Hancer <hancermercedes@gmail.com>
2023-04-23 16:43:11 -0400

    Adding Delete Method to Post logic and adding the Comments Controller and Actions

---
+++ ITMasters.Api/Controllers/CommentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ITMasters.Api.Controllers;
+
+public class CommentController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
